d822b99c037ceedd4674d8587dd3d74480a9a614
s20181105882ASN <s20181105882ASN@mac21.local>
2019-06-12 20:39:17 +0800

    Initial Commit

---
+++ ASN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASN
+//
+//  Created by s20181105882ASN on 2019/6/12.
+//  Copyright © 2019 s20181105882ASN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
