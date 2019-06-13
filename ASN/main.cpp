692e3fc582df3f2493c6e4cdf476a16b16226437
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-06-13 11:08:15 +0800

    yi yi yi
    one

--- ASN/main.cpp
+++ ASN/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  ASN
-//
-//  Created by s20181105882ASN on 2019/6/12.
-//  Copyright © 2019 s20181105882ASN. All rights reserved.
-//
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int a[19];
+    int n;
+    cin>>n;
+    int i,j;
+    for (i=0 ;i<n ; i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0; i<n; i++)
+    {
+        int m;
+        for(j=0; j<n-1-i ; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                m=a[j+1];
+                a[j+1]=a[j];
+                a[j]=m;
+            }
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
